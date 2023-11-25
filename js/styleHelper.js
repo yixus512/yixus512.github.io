e22852de9fff650b7f5fbada3cbf3d81ccfe12ba
logan <logancamp0513@gmail.com>
2023-11-25 17:50:26 -0600

    fixed issue with selected nav box not fitting correctly with the navbar using JavaScript!

---
+++ js/styleHelper.js
@@ -0,0 +1,29 @@
+function resizeSelected(){
+    const SELECTED = document.querySelector('#selected');
+    const HEADER = document.querySelector('header');
+    let selected = document.getElementById('selected');
+
+    const DEFAULT_HEIGHT = 19;
+
+    let height = SELECTED.offsetHeight;
+    let headerHeight = HEADER.offsetHeight;
+
+    let totalNeededPadding = headerHeight - DEFAULT_HEIGHT;
+    let halfNeededPadding = totalNeededPadding / 2;
+
+    /*
+    console.log(height);
+    console.log(headerHeight);
+    console.log(totalNeededPadding);
+    console.log(halfNeededPadding);
+    */
+
+    let setPadding = halfNeededPadding + "px";
+
+    selected.style.paddingTop=setPadding;
+    selected.style.paddingBottom=setPadding;
+
+}
+
+window.addEventListener("resize", resizeSelected);
+window.addEventListener('DOMContentLoaded', resizeSelected);
